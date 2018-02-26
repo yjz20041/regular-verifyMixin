9e3b081025af412c36f9e0c5614eaa5703b0dc36
杨介正 <yangjiezheng@yangjiezhengdeMacBook-Pro.local>
2018-02-26 11:29:51 +0800

    flag them if the callback has already called

--- src/multileVerifyMixin.js
+++ src/multileVerifyMixin.js
@@ -9,9 +9,17 @@ export default (app) => {
         if (!fields.length) {
             return cb(result);
         }
-        fields.forEach( field => {
+        var called = {};
+        fields.forEach( (field, i) => {
             field.verify(null, ret => {
-                count++;
+                // the cb may be called more than once unintentional 
+                // in the pattern of rule, which will cause the called
+                // number of the callback incresing. so, we flag them if
+                // the callback has already called.
+                if (called[i] !== true) {
+                    count++;
+                    called[i] = true;
+                } 
                 result.message[ret.name] = ret;
                 values[ret.name] = ret.value;
                 if (ret.result === false) {
@@ -27,8 +35,3 @@ export default (app) => {
         })
     }
 }
-
-
-
-
-
